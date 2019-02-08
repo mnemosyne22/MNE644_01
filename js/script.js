7aee64d4a73da27cc8f8dedef868d164a7ca8fcd
mnemosyne22 <47236255+mnemosyne22@users.noreply.github.com>
2019-02-07 21:57:29 -0800

    Assignment 1

---
+++ js/script.js
@@ -0,0 +1,158 @@
+/*eslint-env browser*/
+
+//STEP 1 Convert the following identifiers to Camel Case notation
+/*
+var someMonth;
+function theMonth();
+var currentMonth;
+var summerMonth;
+var MyLibraryFunction;
+
+// STEP 2 Examples of Literal Expressions
+
+5.4         // numeric literal expression
+"Aloha!"    // string literal expression
+false       // Boolean literal expression
+null        // null literal expression
+
+// STEP 3 Examples of Complex or Variable Expressions
+
+var myExpression = 8 * (3 / 4) + 3;
+var hisExpression = myExpression + "%" + myExpression;
+
+// STEP 4 Declare Identifiers
+
+var firstName;
+var lastName;
+var strAddress;
+var strCity;
+var strState;
+var numZipCode;
+var numYourAge;
+var referralSource;
+var mayWeContactYou;
+
+// STEP 5 Methods for declaring and assigning values 
+
+var firstName;
+var lastName;
+firstName = "Jackie";
+lastName = "Brown";
+
+var strAddress = "333 Ocean Boulevard";
+var strCity = "Newport Beach";
+var strState = "CA";
+var numZipCode = 92603;
+
+var firstName = "Jackie", referralSource = true;
+var firstName = "Jackie", mayWeContactYou = true;
+
+// STEP 6 Create a variable, add number and string, and display coerced result in console window
+
+var myTrips = 18;
+window.console.log("Jackie managed to smuggle dope on " + myTrips + " intercontinental flights.");
+
+// STEP 7 Create two variables
+
+var isFlightAttendant = true + " Jackie is a flight attendant.";
+window.console.log(isFlightAttendant);
+
+var nextFlight = true + 18;
+window.console.log(nextFlight);
+
+// STEP 8
+
+var someString = "Who once said, 'Only two things are infinite, the universe and human stupidity, and I'm not sure about the former.'";
+window.console.log(someString);
+
+// STEP 9
+
+var j = null;
+window.console.log(j);
+
+// STEP 10 typeof operator 
+
+typeof "Brown"                  
+typeof 4.58                   
+typeof false                  
+typeof myTrips                  
+typeof null                   
+
+// STEP 11
+
+var hisName = "Zak Ruvalcaba";
+hisName = "Jackie Brown";
+window.alert("Hello, " + hisName + ", welcome to JavaScript class!");
+
+// STEP 12
+
+var name = "Jackie Brown";
+window.alert("Hello, " + name + ", welcome to JavaScript class!");
+
+// STEP 13
+
+var name = "Jackie Brown";
+var course = "JavaScript";
+wondow.alert("Hello, " + name + ", welcome to " + course + " class!");
+
+// STEP 14
+
+var name = "Jackie Brown";
+var course = "JavaScript";
+window.alert("Hello, " + name + ".\nWelcome to " + course + " class!");
+
+// STEP 15 & 16
+
+var name = prompt("What is your name?");
+var course = prompt("What is the name of this class?"); 
+window.alert("Hello, " + name + ". Welcome to " + course + " class!");
+
+// STEP 17
+
+var x = 10;
+var y = 20;
+window.console.log(x + y);
+
+// STEP 18
+
+var x = 20;
+window.console.log(x + 20);
+
+// STEP 19
+
+var x = 20;
+window.console.log(x * 5);
+
+// STEP 20
+
+var x = (20 % 3);
+window.console.log(x / 1);
+
+// STEP 21
+
+var q = 5;
+var k = 7;
+window.console.log(q <= k);
+
+// STEP 22
+
+var r = 10;
+var s = 3;
+window.console.log(r == s);
+
+// STEP 23
+
+var widget = new Object();
+window.console.log(typeof widget);
+
+// STEP 24
+
+var widget = new Object();
+window.console.log(widget instanceof Object));
+
+
+
+// STEP 25
+
+var widget = new Object();
+window.console.log(widget instanceof Array));
